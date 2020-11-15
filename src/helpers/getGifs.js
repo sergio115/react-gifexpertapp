0dc04f75c7a221ac657935820f5941127d6a9930
Sergio Eduardo Pérez Rosales <39928687+sergio115@users.noreply.github.com>
2020-11-15 16:37:15 -0600

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,18 @@
+import { GifGridItem } from "../components/GifGridItem";
+
+export const getGifs = async( category ) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=TNhtlCZiLaweTP0zCmRekmlpt2NVrPd8`;
+    const resp = await fetch( url );
+    const { data } = await resp.json();
+
+    const gifs = data.map( img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url,
+        }
+    });
+
+    return gifs;
+};
